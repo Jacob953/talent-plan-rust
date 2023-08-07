f551041757f4cb95aeb8302d263518b4cc30c12e
accce953 <i@jacob953.com>
2023-08-08 01:54:20 +0800

    feat: build crate kvs with offcial project-1/tests
    Signed-off-by: accce953 <i@jacob953.com>

---
+++ src/kv.rs
@@ -0,0 +1,35 @@
+// MIT License
+//
+// Copyright (c) 2023 Chunfung
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+use std::collections::HashMap;
+
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {
+            map: HashMap::new(),
+        }
+    }
+
+    pub fn set(&mut self, key: String, value: String) {}
+
+    pub fn get(&self, key: String) -> Option<String> {
+        None
+    }
+
+    pub fn remove(&mut self, key: String) {}
+}
